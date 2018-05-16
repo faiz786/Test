09874c93460f3e18318968762b9e5a8d9195ac4a
Md. Faizan shaikh <Faizans@rediff.net>
2018-05-16 17:52:30 +0530

    first commit

---
+++ app/src/main/java/com/rediff/test/MainActivity.java
@@ -0,0 +1,13 @@
+package com.rediff.test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
